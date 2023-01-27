f2ce7088e71925518083cdb340b688cbd2772f91
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-27 13:53:56 +0000

    removing method and adding method for preparing inputs from a directory of phenopackets

--- src/pheval_phen2gene/prepare/prepare_inputs.py
+++ src/pheval_phen2gene/prepare/prepare_inputs.py
@@ -1,26 +1,27 @@
 from pathlib import Path
-from typing import List
 
+from pheval.utils.file_utils import all_files
 from pheval.utils.phenopacket_utils import PhenopacketUtil, phenopacket_reader
+from phenopackets import PhenotypicFeature
 
 
-def extract_proband_hpo_ids(phenopacket_path: Path) -> List[str]:
-    """Extract a list of HPO ids used to describe the proband's phenotypic profile in a phenopacket."""
-    phenopacket = phenopacket_reader(phenopacket_path)
-    phenotypic_profile = PhenopacketUtil(phenopacket).observed_phenotypic_features()
-    hpo_ids = [hpo.type.id for hpo in phenotypic_profile]
-    return hpo_ids
-
-
-def write_hpo_ids_to_input_file(output_file: Path, hpo_ids: List[str]) -> None:
+def write_hpo_ids_to_output_file(output_file: Path, phenotypic_profile: [PhenotypicFeature]) -> None:
     """Write a list of HPO ids to a new text file for input into Phen2Gene."""
     with open(output_file, "w") as output:
-        output.write("\n".join(hpo_ids))
+        output.write("\n".join([hpo.type.id for hpo in phenotypic_profile]))
     output.close()
 
 
 def prepare_input(output_dir: Path, phenopacket_path: Path) -> None:
     """Prepare the text file input for Phen2Gene."""
-    hpo_ids = extract_proband_hpo_ids(phenopacket_path)
+    output_dir.mkdir(exist_ok=True)
+    phenopacket = phenopacket_reader(phenopacket_path)
+    phenotypic_profile = PhenopacketUtil(phenopacket).observed_phenotypic_features()
     output_file_path = output_dir.joinpath(phenopacket_path.name + ".txt")
-    write_hpo_ids_to_input_file(output_file_path, hpo_ids)
+    write_hpo_ids_to_output_file(output_file_path, phenotypic_profile)
+
+
+def prepare_inputs(output_dir: Path, phenopacket_dir: Path) -> None:
+    """Prepare text files input for Phen2Gene from a directory of phenopackets."""
+    for phenopacket_path in all_files(phenopacket_dir):
+        prepare_input(output_dir, phenopacket_path)
