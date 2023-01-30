c62a4facc35a55595c296a689e838e00e09c43ad
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-30 19:06:31 +0000

    specifying output in docstring

--- src/pheval_phen2gene/post_process/post_process.py
+++ src/pheval_phen2gene/post_process/post_process.py
@@ -5,7 +5,7 @@ from pheval_phen2gene.post_process.post_process_results_format import create_sta
 
 
 def post_process_results_format(testdata_dir: Path, output_dir: Path):
-    """Create pheval gene result from Phen2Gene output."""
+    """Create pheval gene result from Phen2Gene tsv output."""
     print("...standardising results format...")
     run_output_dir = Path(output_dir).joinpath(
         f"phen2gene" f"{os.sep}{os.path.basename(testdata_dir)}_results"
