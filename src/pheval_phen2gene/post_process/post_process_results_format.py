bba2d0c3881454ce00e6535bed910ea77a636e51
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-01-30 19:33:17 +0000

    updating docstrings

--- src/pheval_phen2gene/post_process/post_process_results_format.py
+++ src/pheval_phen2gene/post_process/post_process_results_format.py
@@ -7,6 +7,7 @@ from pheval.utils.phenopacket_utils import GeneIdentifierUpdater, create_hgnc_di
 
 
 def read_phen2gene_result(phen2gene_result: Path):
+    """Read Phen2Gene tsv output."""
     return pd.read_csv(phen2gene_result, delimiter="\t")
 
 
@@ -16,9 +17,11 @@ class StandardisePhen2GeneResult:
         self.gene_identifier_updater = gene_identifier_updater
 
     def add_ensembl_identifier(self, gene_symbol: str):
+        """Add ensembl identifier to results."""
         return self.gene_identifier_updater.find_identifier(gene_symbol)
 
     def create_result_dictionary(self, result_entry: pd.Series):
+        """Create a basic result structure with gene symbol, gene identifier, and score."""
         return {
             "gene_symbol": result_entry["Gene"],
             "gene_identifier": self.add_ensembl_identifier(result_entry["Gene"]),
@@ -26,12 +29,14 @@ class StandardisePhen2GeneResult:
         }
 
     def simplify_result(self, standardised_result: []):
+        """Create a simplified result structure."""
         for _index, row in self.gene_result.iterrows():
             standardised_result.append(self.create_result_dictionary(row))
         return standardised_result
 
     @staticmethod
     def sort_result(standardised_result: []):
+        """Sort result in descending order."""
         return sorted(
             standardised_result,
             key=lambda d: d["score"],
@@ -40,6 +45,7 @@ class StandardisePhen2GeneResult:
 
     @staticmethod
     def add_ranks(standardised_result: []):
+        """Add ranks to results."""
         rank, count, previous = 0, 0, None
         for result in standardised_result:
             count += 1
@@ -51,6 +57,7 @@ class StandardisePhen2GeneResult:
         return standardised_result
 
     def standardise_gene_result(self):
+        """Create PhEval standardised gene result."""
         result = []
         simplified_result = self.simplify_result(result)
         sorted_result = self.sort_result(simplified_result)
@@ -58,6 +65,7 @@ class StandardisePhen2GeneResult:
 
 
 def create_standardised_results(output_dir: Path, results_dir: Path) -> None:
+    """Convert Phen2Gene default tsv results to PhEval gene results."""
     output_dir.joinpath("pheval_gene_results/").mkdir(exist_ok=True)
     hgnc_dict = create_hgnc_dict()
     gene_identifier_updater = GeneIdentifierUpdater(hgnc_dict, "ensembl_id")
@@ -95,4 +103,5 @@ def create_standardised_results(output_dir: Path, results_dir: Path) -> None:
     type=Path,
 )
 def post_process_phen2gene_results(results_dir: Path, output_dir: Path) -> None:
+    """Post-process Phen2Gene .tsv results to PhEval gene result format."""
     create_standardised_results(output_dir, results_dir)
