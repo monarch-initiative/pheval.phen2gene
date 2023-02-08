6e7b301950ab21f203a3f1550e6758ff6fb5afe7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-02-08 12:32:06 +0000

    fixing tests

--- tests/test_post_process_results_format.py
+++ tests/test_post_process_results_format.py
@@ -1,117 +1,86 @@
 import unittest
 
 import pandas as pd
+from pheval.post_processing.post_processing import PhEvalGeneResult, RankedPhEvalGeneResult
 from pheval.utils.phenopacket_utils import GeneIdentifierUpdater, create_hgnc_dict
 
-from pheval_phen2gene.post_process.post_process_results_format import StandardisePhen2GeneResult
+from pheval_phen2gene.post_process.post_process_results_format import (
+    PhEvalGeneResultFromPhen2GeneTsvCreator,
+    create_pheval_gene_result_from_phen2gene,
+)
+
+example_phen2gene_result = pd.DataFrame(
+    [
+        {"Rank": 1, "Gene": "GCDH", "ID": "2639", "Score": 1.0, "Status": "SeedGene"},
+        {"Rank": 2, "Gene": "ETFB", "ID": "2109", "Score": 0.298386, "Status": "SeedGene"},
+        {"Rank": 3, "Gene": "ETFA", "ID": "2108", "Score": 0.286989, "Status": "SeedGene"},
+    ]
+)
 
 
-class TestStandardisePhen2GeneResult(unittest.TestCase):
-    result = None
-
+class TestPhEvalGeneResultFromPhen2GeneTsvCreator(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        hgnc_dict = create_hgnc_dict()
-        gene_identifier_updater = GeneIdentifierUpdater(hgnc_dict, "ensembl_id")
-        cls.result = [
-            {"Rank": 1, "Gene": "GCDH", "ID": "2639", "Score": 1.0, "Status": "SeedGene"},
-            {"Rank": 2, "Gene": "ETFB", "ID": "2109", "Score": 0.298386, "Status": "SeedGene"},
-            {"Rank": 3, "Gene": "ETFA", "ID": "2108", "Score": 0.286989, "Status": "SeedGene"},
-        ]
-        cls.standardise_result = StandardisePhen2GeneResult(
-            gene_result=pd.DataFrame(cls.result), gene_identifier_updater=gene_identifier_updater
-        )
-        cls.standardised_result = []
-
-    def test_add_ensembl_identifier(self):
-        self.assertEqual(
-            self.standardise_result.add_ensembl_identifier(("GCDH")), "ENSG00000105607"
+        cls.phen2gene_result = PhEvalGeneResultFromPhen2GeneTsvCreator(
+            phen2gene_tsv_result=example_phen2gene_result,
+            gene_identifier_updator=GeneIdentifierUpdater(create_hgnc_dict(), "ensembl_id"),
         )
 
-    def test_create_result_dictionary(self):
+    def test_find_gene_symbol(self):
         self.assertEqual(
-            self.standardise_result.create_result_dictionary(pd.Series(self.result[0])),
-            {"gene_symbol": "GCDH", "gene_identifier": "ENSG00000105607", "score": 1.0},
+            self.phen2gene_result.find_gene_symbol(example_phen2gene_result.iloc[0]), "GCDH"
         )
 
-    def test_simplify_result(self):
+    def test_find_ensembl_identifier(self):
         self.assertEqual(
-            self.standardise_result.simplify_result(self.standardised_result),
+            self.phen2gene_result.find_ensembl_identifier(example_phen2gene_result.iloc[0]),
+            "ENSG00000105607",
+        )
+
+    def test_find_relevant_score(self):
+        self.assertEqual(
+            self.phen2gene_result.find_relevant_score(example_phen2gene_result.iloc[0]), 1.0
+        )
+
+    def test_extract_pheval_gene_requirements(self):
+        self.assertEqual(
+            self.phen2gene_result.extract_pheval_gene_requirements(),
             [
-                {"gene_symbol": "GCDH", "gene_identifier": "ENSG00000105607", "score": 1.0},
-                {"gene_symbol": "ETFB", "gene_identifier": "ENSG00000105379", "score": 0.2984},
-                {"gene_symbol": "ETFA", "gene_identifier": "ENSG00000140374", "score": 0.287},
+                PhEvalGeneResult(gene_symbol="GCDH", gene_identifier="ENSG00000105607", score=1.0),
+                PhEvalGeneResult(
+                    gene_symbol="ETFB", gene_identifier="ENSG00000105379", score=0.2984
+                ),
+                PhEvalGeneResult(
+                    gene_symbol="ETFA", gene_identifier="ENSG00000140374", score=0.287
+                ),
             ],
         )
 
-    def test_sort_result(self):
+
+class TestCreatePhEvalGeneResultFromPhen2Gene(unittest.TestCase):
+    def test_create_pheval_gene_result_from_phen2gene(self):
         self.assertEqual(
-            self.standardise_result.sort_result(
-                [
-                    {"gene_symbol": "GCDH", "gene_identifier": "ENSG00000105607", "score": 1.0},
-                    {"gene_symbol": "ETFB", "gene_identifier": "ENSG00000105379", "score": 0.2984},
-                    {"gene_symbol": "ETFA", "gene_identifier": "ENSG00000140374", "score": 0.287},
-                ]
+            create_pheval_gene_result_from_phen2gene(
+                example_phen2gene_result, GeneIdentifierUpdater(create_hgnc_dict(), "ensembl_id")
             ),
             [
-                {"gene_symbol": "GCDH", "gene_identifier": "ENSG00000105607", "score": 1.0},
-                {"gene_symbol": "ETFB", "gene_identifier": "ENSG00000105379", "score": 0.2984},
-                {"gene_symbol": "ETFA", "gene_identifier": "ENSG00000140374", "score": 0.287},
-            ],
-        )
-
-    def test_add_ranks(self):
-        self.assertEqual(
-            self.standardise_result.add_ranks(
-                [
-                    {"gene_symbol": "GCDH", "gene_identifier": "ENSG00000105607", "score": 1.0},
-                    {"gene_symbol": "ETFB", "gene_identifier": "ENSG00000105379", "score": 0.2984},
-                    {"gene_symbol": "ETFA", "gene_identifier": "ENSG00000140374", "score": 0.287},
-                ]
-            ),
-            [
-                {
-                    "gene_symbol": "GCDH",
-                    "gene_identifier": "ENSG00000105607",
-                    "score": 1.0,
-                    "rank": 1,
-                },
-                {
-                    "gene_symbol": "ETFB",
-                    "gene_identifier": "ENSG00000105379",
-                    "score": 0.2984,
-                    "rank": 2,
-                },
-                {
-                    "gene_symbol": "ETFA",
-                    "gene_identifier": "ENSG00000140374",
-                    "score": 0.287,
-                    "rank": 3,
-                },
-            ],
-        )
-
-    def test_standardise_gene_result(self):
-        self.assertEqual(
-            self.standardise_result.standardise_gene_result(),
-            [
-                {
-                    "gene_symbol": "GCDH",
-                    "gene_identifier": "ENSG00000105607",
-                    "score": 1.0,
-                    "rank": 1,
-                },
-                {
-                    "gene_symbol": "ETFB",
-                    "gene_identifier": "ENSG00000105379",
-                    "score": 0.2984,
-                    "rank": 2,
-                },
-                {
-                    "gene_symbol": "ETFA",
-                    "gene_identifier": "ENSG00000140374",
-                    "score": 0.287,
-                    "rank": 3,
-                },
+                RankedPhEvalGeneResult(
+                    pheval_gene_result=PhEvalGeneResult(
+                        gene_symbol="GCDH", gene_identifier="ENSG00000105607", score=1.0
+                    ),
+                    rank=1,
+                ),
+                RankedPhEvalGeneResult(
+                    pheval_gene_result=PhEvalGeneResult(
+                        gene_symbol="ETFB", gene_identifier="ENSG00000105379", score=0.2984
+                    ),
+                    rank=2,
+                ),
+                RankedPhEvalGeneResult(
+                    pheval_gene_result=PhEvalGeneResult(
+                        gene_symbol="ETFA", gene_identifier="ENSG00000140374", score=0.287
+                    ),
+                    rank=3,
+                ),
             ],
         )
