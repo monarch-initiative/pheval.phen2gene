efd1acbf9a1cdf4e2e16d10c1de5b2035c98f126
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-03-24 15:24:20 +0000

    removing manual path creation

--- src/pheval_phen2gene/post_process/post_process.py
+++ src/pheval_phen2gene/post_process/post_process.py
@@ -1,19 +1,15 @@
-import os
 from pathlib import Path
 
 from pheval_phen2gene.config_parser import Phen2GeneConfig
 from pheval_phen2gene.post_process.post_process_results_format import create_standardised_results
 
 
-def post_process_results_format(testdata_dir: Path, output_dir: Path, config: Phen2GeneConfig):
+def post_process_results_format(raw_results_dir: Path, output_dir: Path, config: Phen2GeneConfig):
     """Create pheval gene result from Phen2Gene tsv output."""
     print("...creating pheval gene results format...")
-    run_output_dir = Path(output_dir).joinpath(
-        f"phen2gene" f"{os.sep}{os.path.basename(testdata_dir)}_results"
-    )
     create_standardised_results(
-        results_dir=Path(run_output_dir).joinpath("phen2gene_results"),
-        output_dir=run_output_dir,
+        results_dir=raw_results_dir,
+        output_dir=output_dir,
         sort_order=config.post_process.score_order,
     )
     print("done")
