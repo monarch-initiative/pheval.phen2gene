c0630cc387667a126a928b2dce054a7c00360efb
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-26 12:39:01 +0100

    add dataclasses for tool_specific_configuration_options field

---
+++ src/pheval_phen2gene/tool_specific_configuration_parser.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+from pydantic import BaseModel, Field
+
+
+class PostProcessing(BaseModel):
+    score_order: str = Field(...)
+
+
+class Phen2GeneToolSpecificConfigurations(BaseModel):
+    environment: str = Field(...)
+    phen2gene_python_executable: Path = Field(...)
+    post_process: PostProcessing = Field(...)
